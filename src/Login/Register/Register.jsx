6fa858afe6233d42619afcfe24c93c7cd80ced69
sayeed083 <sayeedislam407@gmailcom>
2023-05-02 12:56:58 +0600

    Registration Form Designed

--- src/Login/Register/Register.jsx
+++ src/Login/Register/Register.jsx
@@ -7,40 +7,90 @@ const Register = () => {
         <div className='w-25 mx-auto my-5'>
             <h2 className='text-center'>Please Register</h2>
             <Form>
+
+                {/* ------------------
+                        Name Side               
+                ------------------- */}
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Name</Form.Label>
         <Form.Control type="text" name='name' placeholder="Your Name" required />
         
+
+                    {/* ------------------
+                        Email Side               
+                ------------------- */}
+
+
+
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" name='email' placeholder="Enter email" required />
-        
-      </Form.Group>
+        </Form.Group>
+
       
+                {/* ------------------
+                        Password Side               
+                ------------------- */}
+                
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" name='password' placeholder="Password" required />
       </Form.Group>
+
+
+
+                {/* ------------------
+                        Photo Side               
+                ------------------- */}
+
+
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Photo URL</Form.Label>
         <Form.Control type="text" name='photo' placeholder="Your Photo URL" required />
-        
       </Form.Group>
+
+
+                    {/* ------------------
+                        Check-Accept Side               
+                ------------------- */}
+
+
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" name='accept' label="Accept Terms And Conditions" />
       </Form.Group>
+
+
+                {/* ------------------
+                        Button Side               
+                ------------------- */}
+
+
       <Button variant="primary" type="submit">
         Login
       </Button>
+
+
       <br />
+
+                {/* ------------------
+                        Other Sides               
+                ------------------- */}
+
+
       <Form.Text className="text-success">
           Already Have an Account ? <Link className='text-decoration-none' to="/login">Login</Link>
         </Form.Text>
+
+                {/* ------------------
+                        Alert Side               
+                ------------------- */}
+
       <Form.Text className="text-success">
           
         </Form.Text>
+
       <Form.Text className="text-danger">
           
         </Form.Text>
